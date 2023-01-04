b91c303ca21359644d2fedeb266e5199052d8300
sema <tksema50@gmail.com>
2023-01-04 20:57:32 +0300

    first commit

---
+++ src/Question01/ArmstrongNumber.java
@@ -0,0 +1,13 @@
+package Question01;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        /*
+         * Java 8 / Lambda approach to generate Armstrong number.
+         * Armstrong always start to look from number 1.
+         * @param series Number of how many Armstrong number should be generated
+         * @return List holding resulting Armstrong number.
+         */
+
+    }
+}
