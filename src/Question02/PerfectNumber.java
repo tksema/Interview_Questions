efa012fe20623ea1ec946a1ac8dfc8703323a9c7
sema <tksema50@gmail.com>
2023-01-05 00:07:10 +0300

    first commit

--- src/Question02/PerfectNumber.java
+++ src/Question02/PerfectNumber.java
@@ -1,5 +1,9 @@
 package Question02;
 
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class PerfectNumber {
  /* Perfect Number (Mukemmel sayi)
       Kullanici tarafindan bir sayiya kadar olan sayilarin,
@@ -10,4 +14,23 @@ public class PerfectNumber {
       INPUT      : 6
       OUTPUT     :  1,2,3
     */
+ public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     System.out.print("bir sayi giriniz :");
+     int girilenSayi = scan.nextInt();
+     perfectNumber(girilenSayi);
+
+ }
+    private static void perfectNumber(int girilenSayi) {
+        IntStream.range(1, girilenSayi)
+                .filter(n -> tamBolenler(n).sum() == n)
+                .forEach(n -> System.out.println(n + " mükemmel sayıdır ve tam bölenleri şunlardır: "
+                        + tamBolenler(n).mapToObj(Integer::toString).collect(Collectors.joining(" "))));
+    }
+
+    private static IntStream tamBolenler(int n) {
+        return IntStream.range(1, n).filter(d -> n % d == 0);
+    }
+
+
 }
