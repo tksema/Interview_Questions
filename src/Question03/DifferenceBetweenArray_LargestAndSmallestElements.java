160a7796f7e2497723ab1c925c6f357245f9cc69
sema <tksema50@gmail.com>
2023-01-06 18:57:56 +0300

    first commit

---
+++ src/Question03/DifferenceBetweenArray_LargestAndSmallestElements.java
@@ -0,0 +1,21 @@
+package Question03;
+
+import java.util.Scanner;
+
+public class DifferenceBetweenArray_LargestAndSmallestElements {
+    public static void main(String[] args) {
+         /*
+ Create a function that takes an array and the difference between the largest
+ and the smallest numbers.
+ Ask user to enter array elements.
+    kullanıcının girdigi bir array'in en buyuk elemani ile
+    en kucuk elemanının  farkını bulan bir method create ediniz.
+ */
+
+
+    }
+
+
+
+}
+
