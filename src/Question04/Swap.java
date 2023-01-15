517556c8322310b9471c9a857b90c4d39c160ecd
sema <tksema50@gmail.com>
2023-01-15 10:17:33 +0300

    first commit

--- src/Question04/Swap.java
+++ src/Question04/Swap.java
@@ -4,21 +4,29 @@ import java.util.Scanner;
 
 public class Swap {
 
-    //Write a Java Program to swap two numbers
+    //İki sayinin yerlerini degistiren bir Java programi yaziniz
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 2 numbers to swap");
+        System.out.println("Lutfen degistirilecek iki sayi giriniz");
         double n1 = scan.nextDouble();
         double n2 = scan.nextDouble();
 
-        System.out.println("Before swapping: " + n1 + " - " + n2);
+        System.out.println("Swap'tan once: " + n1 + " - " + n2);
 
 
-        //1.Way: Use 3rd variable
+        //1.Yol: 3. bir variable kullanalim
         double temp = n1;
         n1 = n2;
         n2 = temp;
-        System.out.println("After swapping: " + n1 + " - " + n2);
+        System.out.println("Swap'tan sonra: " + n1 + " - " + n2);
+
+
+        //2.Yol: 3.bir variable kullanmadan
+        n1 = n1 + n2;
+        n2 = n1 - n2;
+        n1 = n1 - n2;
+        System.out.println("Swap'tan sonra: " + n1 + " - " + n2);
+
 
     }
 }
