ea42514fd51484418b66ee3fad519ca49d69c826
sema <tksema50@gmail.com>
2023-01-15 10:10:58 +0300

    first commit

--- src/Question04/Swap.java
+++ src/Question04/Swap.java
@@ -1,9 +1,16 @@
 package Question04;
 
+import java.util.Scanner;
+
 public class Swap {
 
     //Write a Java Program to swap two numbers
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 2 numbers to swap");
+        double n1 = scan.nextDouble();
+        double n2 = scan.nextDouble();
 
+        System.out.println("Before swapping: " + n1 + " - " + n2);
     }
 }
